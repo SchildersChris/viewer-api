01bfd4dab466698c3b55d00399c4cc346ee1f6b0
Chris <chris-schilders@hotmail.nl>
2021-02-15 01:31:00 +0100

    Updated tests

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,13 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { User } from './schema/user.schema';
+import { UserModule } from './user.module';
 
 describe('UserService', () => {
   let service: UserService;
 
+  const mockRepository = {
+    find() {
+      return {};
+    }
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService]
-    }).compile();
+      imports: [UserModule]
+    })
+      .overrideProvider(getModelToken(User.name))
+      .useValue(mockRepository)
+      .compile();
 
     service = module.get<UserService>(UserService);
   });
