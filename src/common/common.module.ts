e3b7b77dd520cbd3eb74f0a5ac2874e45da656b4
Chris <chris-schilders@hotmail.nl>
2021-02-17 23:04:11 +0100

    Updated common module export

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { SeedModule } from './seed/seed.module';
 
 @Module({
-  imports: [SeedModule],
-  exports: [SeedModule]
+  imports: [SeedModule]
 })
 export class CommonModule {}
