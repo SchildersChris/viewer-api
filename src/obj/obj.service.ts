9bf5fc4e6cb447d3fcd1efdd3e56cbfc062604e9
Chris <chris-schilders@hotmail.nl>
2021-03-03 01:12:18 +0100

    Updated service and cleanup

--- src/obj/obj.service.ts
+++ src/obj/obj.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model, ObjectId } from 'mongoose';
+import { Model } from 'mongoose';
 import { Obj, ObjDocument } from './schema/obj.schema';
 import { CreateObjDto } from './dto/create-obj.dto';
 
@@ -24,6 +24,6 @@ export class ObjService {
   }
 
   async findAll(): Promise<Obj[]> {
-    return this.objModel.find().exec();
+    return this.objModel.find({}, { name: 1 }).exec();
   }
 }
